8608f54f9be104a476238be058048da0ac5e18d4
yang3yen <yang3yen@gmail.com>
2017-09-26 18:55:38 +0800

    add class property

--- pylib/decorator.py
+++ pylib/decorator.py
@@ -1,20 +1,27 @@
 
 
-import functools
-
-
-__all__ = ['cached_property']
+__all__ = ['cached_property', 'class_property']
 
 
 class cached_property(object):
     """cached property"""
-    def __int__(self, func):
-        functools.update_wrapper(self, func)
+    def __init__(self, func):
         self.func = func
 
-    def __get__(self, instance, cls):
+    def __get__(self, instance, owner):
         if instance is None:
             return self
         value = self.func(instance)
         instance.__dict__[self.func.__name__] = value
+        return value
+
+
+class class_property(object):
+    """class property"""
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, owner):
+        value = self.func(owner)
+        setattr(owner, self.func.__name__, value)
         return value
